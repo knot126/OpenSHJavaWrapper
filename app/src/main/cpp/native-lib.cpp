aac42daced2fc4dd228a9d9b060018ba4f3552ca
Your Name <you@example.com>
2024-08-06 22:28:58 -0500

    Minor cleanup

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,9 +2,6 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_mediocre_smashhit_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+Java_com_mediocre_smashhit_MainActivity_stringFromJNI(JNIEnv* env, jobject obj) {
+    return env->NewStringUTF("You need to replace this file with libsmashhit.so!");
 }
--- app/src/main/java/com/mediocre/smashhit/Main.java
+++ app/src/main/java/com/mediocre/smashhit/Main.java
@@ -52,7 +52,11 @@ public class Main extends NativeActivity {
     public void setSystemUiFlags() {
         if (Build.VERSION.SDK_INT >= 19) {
             try {
-                getWindow().getDecorView().setSystemUiVisibility(5894);
+                // Should be 5894
+                getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN |View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
             } catch (Exception e) {
                 System.out.println(e);
             }
